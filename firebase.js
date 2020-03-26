69854857cbe3b944d2c8472d1e43fe9d9d835935
Nonlapan <nonlapan.1998@mail.kmutt.ac.th>
2020-03-26 14:56:48 +0700

    [ADD] Analyse Result

---
+++ firebase.js
@@ -0,0 +1,25 @@
+<body>
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.12.0/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD1ark3iF8WazBagupGswZWGRzsrdoJ5MM",
+    authDomain: "subjective-well-being.firebaseapp.com",
+    databaseURL: "https://subjective-well-being.firebaseio.com",
+    projectId: "subjective-well-being",
+    storageBucket: "subjective-well-being.appspot.com",
+    messagingSenderId: "445003882411",
+    appId: "1:445003882411:web:492df3948c8fbe488dfcee",
+    measurementId: "G-MW0CJSMJJV"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+</body>
